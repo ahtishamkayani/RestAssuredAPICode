8be4d8ca420f90c94f0fdd6fa74f5815cb236b00
ahtishamkayani <ahtishamkayani1@gmail.com>
2022-04-07 00:12:45 +0500

    Api Code changes

--- src/test/java/commerce_service/Addressbyid.java
+++ src/test/java/commerce_service/Addressbyid.java
@@ -9,7 +9,6 @@ public class Addressbyid extends Createaddress {
 	@Test(priority = 6)
 	public void get_addressby_id() {
 		Data data = new Data();
-
 		given().header("Authorization", accessToken).header("Content-Type", data.contentType)
 				.header("x-site-context", data.siteContext).header("x-api-key", data.apiKey)
 				.get(data.baseUrl + "/user/" + userId + "/address/" + addressId).then().statusCode(200).log().all();
