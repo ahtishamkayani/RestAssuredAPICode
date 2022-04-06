c9020d496088dc219143a4fbf543a73b165ccf52
ahtishamkayani <ahtishamkayani1@gmail.com>
2022-04-07 00:15:42 +0500

    Updated Code with fixes

--- src/test/java/commerce_service/Addressbyid.java
+++ src/test/java/commerce_service/Addressbyid.java
@@ -12,6 +12,5 @@ public class Addressbyid extends Createaddress {
 		given().header("Authorization", accessToken).header("Content-Type", data.contentType)
 				.header("x-site-context", data.siteContext).header("x-api-key", data.apiKey)
 				.get(data.baseUrl + "/user/" + userId + "/address/" + addressId).then().statusCode(200).log().all();
-
 	}
 }
